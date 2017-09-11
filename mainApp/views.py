37f2481c4d162de91c96eff02cdbfae0d220ff29
Ruhshan <ruhshan.ahmed@gmail.com>
2017-09-12 00:22:20 +0600

    working!

--- mainApp/views.py
+++ mainApp/views.py
@@ -8,12 +8,16 @@ from django.views import generic
 from django.contrib.auth.models import Group
 from django.contrib.auth.forms import UserChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
-from rest_framework import serializers
+from rest_framework import serializers, generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import *
 
+class ClientPageNumberPagination(PageNumberPagination):
+    page_size = 4
+    page_size_query_param = 'pageSize'
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,13 +27,14 @@ class ClientSerializer(serializers.ModelSerializer):
 # Create your views here.
 class ClientListView(generic.ListView):
     model = Client
-
     def get_context_data(self, **kwargs):
         context = super(ClientListView, self).get_context_data(**kwargs)
         return context
 
-class ClientJsonView(APIView):
-    def get(self, request, format=None):
-        clients = Client.objects.all()
-        serialized = ClientSerializer(clients, many=True)
-        return Response({'data':serialized.data})
+class ClientJsonView(generics.ListAPIView):
+    serializer_class = ClientSerializer
+    pagination_class = ClientPageNumberPagination
+
+    def get_queryset(self, *args, **kwargs):
+        print self.request.GET
+        return Client.objects.all()
